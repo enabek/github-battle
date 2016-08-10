10be70dfdc214564e9dde00788153b443a8186c0
Ena Bek <enabek@gmail.com>
2016-08-10 18:39:22 -0400

    Add react skeleton

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var HtmlWepbackPlugin = require('html-webpack-plugin');
+var HtmlWepbackPluginConfig = new HtmlWepbackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
+
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+    ]
+  },
+  plugins: [HtmlWepbackPluginConfig]
+}
