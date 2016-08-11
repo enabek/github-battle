945f3616836eafc77ffb88b0f0153e64791a22cd
Ena Bek <enabek@gmail.com>
2016-08-11 14:51:35 -0400

    Install react-router and build out routes

--- app/index.js
+++ app/index.js
@@ -1,66 +1,10 @@
-var USER_DATA = {
-  name: 'Ena Bek',
-  username: 'enabek',
-  image: 'https://avatars0.githubusercontent.com/u/15351103?v=3&s=460'
-};
-
 var React = require('react');
 var ReactDOM = require('react-dom');
+var routes = require('./config/routes');
 
-var ProfilePic = React.createClass({
-  render: function() {
-    return <img src={this.props.imageUrl} style={{height: 150, width: 150}}/>
-  }
-});
 
-var Link = React.createClass({
-  changeURL: function() {
-    window.location.replace(this.props.href)
-  },
-  render: function() {
-    return (
-      <span
-        style={{color: 'blue', cursor: 'pointer'}} 
-        onClick={this.changeURL}>
-        {this.props.childern}
-      </span>
-    )
-  }
-})
-
-var ProfileLink = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <Link href={'https://www.github.com/' + this.props.username}>
-          {this.props.username}
-        </Link>
-      </div>
-    )
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function() {
-    return (
-      <div>{this.props.name}</div>
-    )
-  }
-})
-
-var Avatar = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    )
-  }
-});
 
 ReactDOM.render(
-  <Avatar user={USER_DATA}/>,
+  routes, //routes file is JSX
   document.getElementById('app')
 );
