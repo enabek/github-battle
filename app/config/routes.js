ad40a421fed7f98e1063e544d935dd33c8aec13c
Ena Bek <enabek@gmail.com>
2016-08-12 17:09:45 -0400

    Add prompt container files, form, and routes

--- app/config/routes.js
+++ app/config/routes.js
@@ -4,13 +4,16 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
-var Main = require("../components/Main");
-var Home = require("../components/Home");
+var Main = require('../components/Main');
+var Home = require('../components/Home');
+var PromptContainer = require('../container/PromptContainer');
 
 var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}> //make main component always active
       <IndexRoute component={Home} /> //IndexRoute makes /home active when no other routes are
+      <Route path='playerOne' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' component={PromptContainer} />
     </Route>
   </Router>
 );
