5bf48750665503d0bb52589c558bff8b1a6633dc
Ena Bek <enabek@gmail.com>
2016-08-12 17:45:04 -0400

    Fix typo

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,5 +1,5 @@
 var React = require('react');
-var transparentBg = require('../').transparentBg;
+var transparentBg = require('../styles').transparentBg;
 var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
 
@@ -8,7 +8,7 @@ var Home = React.createClass({
     return (
       <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
         <h1>Github Battle</h1>
-        <p className"lead">Some fancy motto</p>
+        <p className="lead">Some fancy motto</p>
         <Link to='/playerOne'>
           <button type="button" className="btn btn-lg btn-success">Start!</button>
         </Link>
