7c241a8143ceac555d17db9223dd6c2c0767fde6
Ena Bek <enabek@gmail.com>
2016-08-12 17:25:13 -0400

    Add style to form

--- app/config/routes.js
+++ app/config/routes.js
@@ -12,8 +12,8 @@ var routes = (
   <Router history={hashHistory}>
     <Route path='/' component={Main}> //make main component always active
       <IndexRoute component={Home} /> //IndexRoute makes /home active when no other routes are
-      <Route path='playerOne' component={PromptContainer} />
-      <Route path='playerTwo/:playerOne' component={PromptContainer} />
+      <Route path='playerOne' header="Player One" component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header="Player Two" component={PromptContainer} />
     </Route>
   </Router>
 );
